c91257871339b382d2d3e994b9ba405b66f54905
Stoicoi Nicolae <stoicoinicolae4@gmail.com>
2024-04-27 19:58:13 +0300

    b

--- 6 din 49/Program.cs
+++ 6 din 49/Program.cs
@@ -4,26 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace _6_din_49
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Random random = new Random();
+            int w = 3;
             int[] numere = new int[6];
-            
+            int[] numGenerate = new int[6];
+
+
             string stringnumere = Console.ReadLine();
 
             string[] SplitNumere = stringnumere.Split(' ');
                     
-            for(int i = 0;i<6 ;i++)           
+            for(int i = 0; i<6 ;i++)           
             {
                 numere[i] = int.Parse(SplitNumere[i]);
                 
             }
 
+
+            while(true)
+            {
+
+                for (int i = 1; i <= 6; i++)
+                {
+                  numGenerate[i]  = random.Next(1, 49);
+                }
+
+            }
         
-            Console.WriteLine(numere[1]+4);
+     
           
             Console.ReadLine();
             
