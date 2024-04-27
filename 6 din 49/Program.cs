327f882e13affe93ccce3968e79361694852f47e
Stoicoi Nicolae <stoicoinicolae4@gmail.com>
2024-04-27 19:30:46 +0300

    Add project files.

---
+++ 6 din 49/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_din_49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numere = new int[6];
+            
+            string stringnumere = Console.ReadLine();
+
+            string[] SplitNumere = stringnumere.Split(' ');
+                    
+            for(int i = 0;i<6 ;i++)           
+            {
+                numere[i] = int.Parse(SplitNumere[i]);
+                
+            }
+
+        
+            Console.WriteLine(numere[1]+4);
+          
+            Console.ReadLine();
+            
+        }
+    }
+}
